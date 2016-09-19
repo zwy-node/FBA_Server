78e78f35676abe900e9a2238f3f73b477df2f4b8
YYD <yyd-yyd@hotmail.com>
2016-09-19 17:37:54 +0800

    配置信息模块；

--- src/controller/configInfoController.js
+++ src/controller/configInfoController.js
@@ -3,8 +3,12 @@ const co = require('co');
 module.exports = co.wrap(function*(ctx, next){
     if (ctx.params.type == 'fba'){
         yield ctx.render('configInfo/fba', {});
+    }else if (ctx.params.type == 'driverCosts'){
+        yield ctx.render('configInfo/driverCosts', {});
     }else if (ctx.params.type == 'airTransport'){
         yield ctx.render('configInfo/airTransport', {});
+    }else if (ctx.params.type == 'express'){
+        yield ctx.render('configInfo/express', {});
     }else if (ctx.params.type == 'driver'){
         yield ctx.render('configInfo/driver', {});
     }else if (ctx.params.type == 'destinationAddress'){
