9824bcf30ee8384fef91f0fa87b32b44a7efc28a
YYD <yyd-yyd@hotmail.com>
2016-09-27 19:05:28 +0800

    货车司机页面逻辑；

--- src/controller/configInfoController.js
+++ src/controller/configInfoController.js
@@ -190,11 +190,13 @@ var doDriver = function*(ctx, next) {
             {key: 'carIDImage'}                //车证
         ];
         let driverInfo = Utils.verifyAndFillObject(postData, rules);
+        let id = driverInfo.id;
         driverInfo.status = 1;
         driverInfo.createDate = new Date();
         driverInfo.modifiedTime = new Date();
+        delete driverInfo.id;
         console.log(driverInfo)
-        yield configInfoAction.updateDriver(driverInfo);
+        yield configInfoAction.updateDriver(id, driverInfo);
         ctx.response.redirect('/config/driver');
     } else if (ctx.query.action == 'remove') {
         let postData = ctx.request.body;
