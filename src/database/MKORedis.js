fb69ff57628d1e2be6077670e3343c0c21b4bac8
dedede <939067005@qq.com>
2016-10-04 11:07:27 +0800

    1.修改redis 配置

--- src/database/MKORedis.js
+++ src/database/MKORedis.js
@@ -3,8 +3,9 @@
  */
 
 const redis = require('redis');
+const config = require(BASEDIR + '/config/development');
 
-let redisConfig = CONFIG.redis;
+let redisConfig = config.redis;
 let redisClient = redis.createClient(redisConfig.RDS_PORT, redisConfig.RDS_HOST, {auth_pass: redisConfig.RDS_PWD});
 
 module.exports = redisClient;
